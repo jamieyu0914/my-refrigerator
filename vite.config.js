df87d4e0bef895db51ee8deecb53fadbc6930a60
jamieyu0914 <jamieyu0914@gmail.com>
2026-09-04 17:00:27 +0800

    feat: #0904 CI/CD workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-})
+  // GitHub Pages 專案頁面會部署在 /my-refrigerator/ 這個子路徑下，
+  // 開發模式仍維持根目錄，方便本機 npm run dev
+  base: command === 'build' ? '/my-refrigerator/' : '/',
+}))
